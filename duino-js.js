3a59b5f3a2861b981ca479fad444eb158fda550f
Gamecat999 <130594564+Pihacks999@users.noreply.github.com>
2024-08-19 21:40:40 -0700

    Update duino-js.js

--- duino-js.js
+++ duino-js.js
@@ -2,7 +2,7 @@
 const userThreads = navigator.hardwareConcurrency;
 
 // Define function with default username, rigid and amount of threads
-function startMiner(username = `Hoiboy19`, rigid = `Duino-JS`, threads = 1) {
+function startMiner(username = `katfaucet`, rigid = `Duino-JS`, threads = 1) {
     // Validate the amount of threads
     if (threads < 1) {
         threads = 1;
